538be9a3b6576a476db17f720b1a97ade83cb257
Marcelo Amaro <marcelo.amaro@aluno.uece.br>
2022-04-24 20:21:35 -0300

    Questao 5

--- POO/Locadora de Filme/Locadora/src/locadora/Locadora.java
+++ POO/Locadora de Filme/Locadora/src/locadora/Locadora.java
@@ -5,9 +5,13 @@ public class Locadora {
 
     
     public static void main(String[] args) {
-        Filme alugar = new Filme("HOMEM ARANHA");
+        Filme alugar = new Filme("HOMEM ARANHA ");
         alugar.exibirFilme();
         alugar.calcularPreco();
+        
+        Filme alugar1 = new Filme("THE BATMAN ");
+        alugar1.exibirFilme();
+        alugar1.calcularPreco();
         
     }
     
